10077e9b0c15065b284af6e7857ee27f383f3569
Andika Budiargo <andikabudiargo@gmail.com>
2025-10-24 08:57:23 +0700

    Update create-transfer_in.blade.php

--- resources/views/ppic/create-transfer_in.blade.php
+++ resources/views/ppic/create-transfer_in.blade.php
@@ -284,12 +284,6 @@
     }
 </style>
 @push('scripts')
-<!-- jQuery dulu (kalau pakai) -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-<!-- Library html5-qrcode -->
-<script src="https://unpkg.com/html5-qrcode@2.3.7/minified/html5-qrcode.min.js"></script>
-
 <script>
   
   // =====================
