298c0c190794ab0ca1b7920eb261e420b453f74d
Andika Budiargo <andikabudiargo@gmail.com>
2025-11-25 12:34:06 +0700

    update some partial layout

---
+++ resources/views/layouts/header-security.blade.php
@@ -0,0 +1,15 @@
+<!-- [ Header Topbar ] start -->
+<header>
+  <div class="flex items-center h-14 max-sm:px-[15px] px-[25px] grow
+              bg-gradient-to-b from-gray-600 to-blue-950 text-white relative">
+
+
+        <!-- Logo di tengah hanya untuk mobile/tablet -->
+        <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center">
+        <img src="{{ asset('img/logo.png') }}" alt="Logo" class="h-8">
+        </div>
+
+  
+  </div>
+</header>
+<!-- [ Header ] end -->
