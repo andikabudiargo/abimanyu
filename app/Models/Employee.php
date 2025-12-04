ef5b7d96bb972c1cdb5e19a7d45b879e97c66dcd
Andika Budiargo <andikabudiargo@gmail.com>
2025-12-04 14:14:46 +0700

    Update Employee.php

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -7,14 +7,72 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     protected $fillable = [
-        'nik',
+        // Personal Info
         'name',
+        'gender',
+        'birth_date',
+        'birth_place',
+        'religion',
+        'nationality',
+
+        // Family / PTKP
+        'marital_status',
+        'dependents',
+        'spouse_works',
+
+        // Education & Job
+        'last_education',
+        'employee_type',
+        'instansi',
+        'nik',
+        'join_date',
+        'position_id',
+        'department',
+        'section',
+
+        // Contact
+        'address',
+        'provinsi',
+        'city',
+        'kecamatan',
+        'kelurahan',
+        'phone',
+        'phone_emergency_1',
+        'phone_emergency_2',
+        'email',
+
+        // Official Numbers
+        'ktp_number',
+        'kk_number',
+        'npwp_number',
+        'bpjs_ketenagakerjaan',
+        'bpjs_kesehatan',
+        'bank_number',
+        'bank_account_name',
+
+        // Documents / Files
+        'photo_profile',
+        'cv',
+        'ktp_file',
+        'kk_file',
+        'ijazah',
+        'skck',
+        'mcu',
     ];
 
      public function departments()
     {
         return $this->belongsToMany(Department::class, 'department_employee', 'employee_id', 'dept_id');
     }
+public function positions()
+{
+    return $this->belongsToMany(
+        Position::class,
+        'employee_positions', // pivot table
+        'employee_id',        // FK ke employee
+        'position_id'         // FK ke position
+    );
+}
 
     public function distributions()
 {
