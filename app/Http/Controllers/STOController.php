3f8a3820d40e44fc8dbca6789406f5b8bf9e66ce
Andika Budiargo <andikabudiargo@gmail.com>
2025-12-30 16:55:22 +0700

    Update STOController.php

--- app/Http/Controllers/STOController.php
+++ app/Http/Controllers/STOController.php
@@ -13,11 +13,27 @@ use App\Models\Article;
 
 class STOController extends Controller
 {
-   public function index()
-    {
-        // Mengembalikan view resources/views/accounting/bbm.blade.php
-        return view('facility.sto');
+  
+public function index()
+{
+    $currentTime = Carbon::now();
+    $allowedTime = Carbon::tomorrow()->setTime(8, 0, 0); // besok jam 08:00
+
+    if ($currentTime->lt($allowedTime)) {
+        $diffSeconds = $currentTime->diffInSeconds($allowedTime);
+
+        // Hitung hari, jam, menit, detik
+        $days = floor($diffSeconds / 86400);
+        $hours = floor(($diffSeconds % 86400) / 3600);
+        $minutes = floor(($diffSeconds % 3600) / 60);
+        $seconds = $diffSeconds % 60;
+
+        return view('facility.countdown', compact('days', 'hours', 'minutes', 'seconds'));
     }
+
+    // Jika waktu sudah lewat, tampilkan halaman normal
+    return view('facility.sto');
+}
 
   public function create()
 {
