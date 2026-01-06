930f2bfb5d1a29726d7d1e23fe21415bd83ed770
Andika Budiargo <andikabudiargo@gmail.com>
2026-01-06 14:14:44 +0700

    Update sto.blade.php

--- resources/views/facility/sto.blade.php
+++ resources/views/facility/sto.blade.php
@@ -23,7 +23,7 @@
                 <option value="WIP Sanding">WIP Sanding</option>
       <option value="WIP Buffing">WIP Buffing</option>
       <option value="Werate">Werate</option>
-      <option value="WIP Touchup">WIP Touchup</option>
+      <option value="WIP Touch Up">WIP Touchup</option>
                 <option value="OT">OT</option>
                  <option value="Dead Stock CM1">Dead Stock CM1</option>
             </select>
