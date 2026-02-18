38615fafc707b1ec09c8bbb1de67e0b386150cbd
Andika Budiargo <andikabudiargo@gmail.com>
2026-02-18 17:11:25 +0700

    Update detail-daily-inspection.blade.php

--- resources/views/qc/detail-daily-inspection.blade.php
+++ resources/views/qc/detail-daily-inspection.blade.php
@@ -201,7 +201,7 @@
                 </td>
 
                 <td class="p-2 border font-medium">
-                    {{ $defect->defect->defect ?? '-' }}
+                    {{ $defect->defect->category ?? '-' }} - {{ $defect->defect->defect ?? '-' }}
                 </td>
 
                 <td class="p-2 border text-center font-semibold">
