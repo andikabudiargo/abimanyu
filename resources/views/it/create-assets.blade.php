a5cd53b5e0db65f94d6b5b1d92cf9e2a21949f3f
Andika Budiargo <andikabudiargo@gmail.com>
2026-04-13 15:34:48 +0700

    Update create-assets.blade.php

--- resources/views/it/create-assets.blade.php
+++ resources/views/it/create-assets.blade.php
@@ -308,10 +308,7 @@
                                 <label class="block text-sm font-medium text-gray-700 mb-1">
                                     Person in Charge (PIC) <span class="text-red-500">*</span>
                                 </label>
-                                <div class="relative">
-                                    <span class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none z-10">
-                                        <i data-feather="user" class="w-4 h-4 text-gray-400"></i>
-                                    </span>
+                                <div>
                                      <input type="text" name="assigned_to" id="assigned_to"
                                     class="w-full px-3 py-2.5 border border-gray-200 rounded-lg text-sm shadow-sm focus:ring-2 focus:ring-indigo-400">
                                 </div>
