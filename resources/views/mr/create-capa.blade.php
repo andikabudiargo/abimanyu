b3dd9cdff56f83fb5e8afa5b24ae99cec6fb84dc
Andika Budiargo <andikabudiargo@gmail.com>
2026-04-15 14:23:32 +0700

    Update create-capa.blade.php

--- resources/views/mr/create-capa.blade.php
+++ resources/views/mr/create-capa.blade.php
@@ -267,15 +267,25 @@
             Dept. Representative <span class="text-red-600">*</span>
         </label>
 
-        <!-- ✅ vertical stack -->
-        <div class="space-y-3">
-
             <!-- PIC 1 -->
             <select name="dept_representative" id="representative" required
                 class="w-full px-3 py-2.5 border rounded-lg text-sm shadow-sm
                 focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500">
                 <option value="">Auditee 1</option>
             </select>
+    </div>
+     <div class="hidden">
+        <label class="block text-sm font-medium text-gray-700 mb-1">
+            Dept. Representative <span class="text-red-600">*</span>
+        </label>
+
+            <!-- PIC 1 -->
+           
+    </div>
+    <div>
+        <label class="block text-sm font-medium text-gray-700 mb-1">
+            Dept. Representative 2<span class="text-red-600">*</span>
+        </label>
 
             <!-- PIC 2 -->
             <select name="dept_representative_2" id="representative2"
