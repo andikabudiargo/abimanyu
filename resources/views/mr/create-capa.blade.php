a3c5400d2fb25da5338888ae3b9620e98e8c12b2
Andika Budiargo <andikabudiargo@gmail.com>
2026-04-15 14:43:25 +0700

    Update create-capa.blade.php

--- resources/views/mr/create-capa.blade.php
+++ resources/views/mr/create-capa.blade.php
@@ -242,6 +242,9 @@
      </div>
 
 
+   <!-- Department & Representative -->
+<div class="grid grid-cols-1 md:grid-cols-2 gap-5">
+
     <!-- LEFT: Department -->
     <div class="col-span-2">
         <label class="block text-sm font-medium text-gray-700 mb-1">
@@ -268,7 +271,7 @@
             <select name="dept_representative" id="representative" required
                 class="w-full px-3 py-2.5 border rounded-lg text-sm shadow-sm
                 focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500">
-                <option value="">Auditee 1</option>
+              
             </select>
     </div>
     <div>
@@ -280,10 +283,11 @@
             <select name="dept_representative_2" id="representative2"
                 class="w-full px-3 py-2.5 border rounded-lg text-sm shadow-sm
                 focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500">
-                <option value="">Auditee 2</option>
+               
             </select>
 
         </div>
+    </div>
 
     <!-- Finding -->
     <div>
