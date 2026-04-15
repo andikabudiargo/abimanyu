e0af528c0aa0cad99b358237b1daac01a14a7ae0
Andika Budiargo <andikabudiargo@gmail.com>
2026-04-15 15:12:37 +0700

    Update create-capa.blade.php

--- resources/views/mr/create-capa.blade.php
+++ resources/views/mr/create-capa.blade.php
@@ -262,31 +262,29 @@
     </div>
 
 
-    <div>
-        <label class="block text-sm font-medium text-gray-700 mb-1">
-            Dept. Representative <span class="text-red-600">*</span>
-        </label>
+  <div>
+    <label class="block text-sm font-medium text-gray-700 mb-1">
+        Dept. Representative <span class="text-red-600">*</span>
+    </label>
 
-            <!-- PIC 1 -->
-            <select name="dept_representative" id="representative" required
-                class="w-full px-3 py-2.5 border rounded-lg text-sm shadow-sm
-                focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500">
-              
-            </select>
-    </div>
-    <div>
-        <label class="block text-sm font-medium text-gray-700 mb-1">
-            Dept. Representative 2
-        </label>
+    <select name="dept_representative" id="representative"
+        class="w-full px-3 py-2.5 border rounded-lg text-sm shadow-sm
+        focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500">
+        <option></option>
+    </select>
+</div>
 
-            <!-- PIC 2 -->
-            <select name="dept_representative_2" id="representative2"
-                class="w-full px-3 py-2.5 border rounded-lg text-sm shadow-sm
-                focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500">
-               
-            </select>
+<div>
+    <label class="block text-sm font-medium text-gray-700 mb-1">
+        Dept. Representative 2
+    </label>
 
-        </div>
+    <select name="dept_representative_2" id="representative2"
+        class="w-full px-3 py-2.5 border rounded-lg text-sm shadow-sm
+        focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500">
+        <option></option>
+    </select>
+</div>
     </div>
 
     <!-- Finding -->
