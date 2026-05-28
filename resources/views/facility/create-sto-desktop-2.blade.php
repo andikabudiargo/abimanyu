ca7b338339650853ff403aaeeb8f9a4186b9dc3f
Andika Budiargo <87258005+andikabudiargo@users.noreply.github.com>
2026-05-28 11:59:43 +0700

    Update create-sto-desktop-2.blade.php

--- resources/views/facility/create-sto-desktop-2.blade.php
+++ resources/views/facility/create-sto-desktop-2.blade.php
@@ -11,7 +11,7 @@
   $month = str_pad($month, 2, '0', STR_PAD_LEFT);
 
   $stoRange = [
-    'Dead Stock CM1' => [1, 48],
+    'Dead Stock CM1' => [1, 49],
     'Chemical'       => [1000, 1999],
     'Consumable'     => [2000, 2999],
     'Raw Material'   => [3000, 3999],
@@ -19,7 +19,7 @@
     'WIP Sanding'    => [6000, 6999],
     'WIP Touch Up'   => [7000, 7999],
     'Finish Goods'   => [4000, 4999],
-    'OT'             => [51, 999],
+    'OT'             => [50, 999],
     'Werate'         => [8000, 8999],
   ];
 
